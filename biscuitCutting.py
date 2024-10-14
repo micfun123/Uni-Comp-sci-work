dbfd0902b3a664775e7f60443d6f507ae8763c20
Michael Parker <michaelrbparker@protonmail.com>
2024-10-14 09:03:44 +0100

    pracctice exam

---
+++ biscuitCutting.py
@@ -0,0 +1,7 @@
+
+def biscuitCutting():
+    amount_biscuits = int(input("How many biscuits do you want to make? "))
+    radius_biscuits = int(input("What is the radius of the biscuits? "))
+    diamitor_biscuit = radius_biscuits * 2
+    length_mixture = diamitor_biscuit * amount_biscuits
+    print(f"The width is {diamitor_biscuit} and the mixture is {length_mixture} long") 
